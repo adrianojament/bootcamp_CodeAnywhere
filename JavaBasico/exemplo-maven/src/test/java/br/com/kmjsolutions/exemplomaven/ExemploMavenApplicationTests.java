8b2568ce3aa38f796b3db2a0c2dbd3ff937d156d
adriano.ament <adriano.ament@quiver.net.br>
2021-04-18 18:42:14 -0300

    first commit

---
+++ JavaBasico/exemplo-maven/src/test/java/br/com/kmjsolutions/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kmjsolutions.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
